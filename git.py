2bf90368f3bd60e42ebcce51dc35fa7c453fa454
Asror Kenjayev <asrorkenjayev967@gmail.com>
2024-01-26 10:56:33 +0500

    fayllar joylandi

--- git.py
+++ git.py
@@ -1,3 +1,20 @@
-lst=[23,55,67,887,345,345,32,75]
-lst.sort(reverse =True)
-print(lst[2])
+
+# def prifiks_found(st: str,n: int):
+#     current=st[0]
+#     for i in range(1,len(st)):
+#         temp=""
+#         for j in range(len(st[i])):
+#             if j<len(current) and current[j] == st[i][j]:
+#                 temp+=current[j]
+#             else:
+#                 break
+#         current=temp
+#     return(current)   
+            
+    
+# num=int(input("num: "))
+# st=["geeksfor","geeksfor geeks","geek"]
+# print(prifiks_found(st,num))
+
+
+
